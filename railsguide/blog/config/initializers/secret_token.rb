3e2a5470d769da83a746588a4a0804e7c99358a2
¨Xiaofeng <quxiaofeng.at.polyu@gmail.com>
2012-12-12 11:25:14 +0800

    add rails guide
    Signed-off-by: ¨Xiaofeng <¨quxiaofeng.at.polyu@gmail.com¨>

---
+++ railsguide/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '32bc179d2fc8c262f6b2270bede15c18e19fe640be67e3d5d6e4bdcb71e3cedd83bc13011fb601e010501c3a0e5375c81ac9c18927cafe93a6a66251593980c2'

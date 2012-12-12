7b843aa143e683f181803fa567111b49c056c244
¨Xiaofeng <quxiaofeng.at.polyu@gmail.com>
2012-12-12 16:46:19 +0800

    init impro
    Signed-off-by: ¨Xiaofeng <¨quxiaofeng.at.polyu@gmail.com¨>

---
+++ impro/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Impro::Application.config.secret_token = '9e6020aedf6254f7b5cf3afae7b396094aa56f6baa12c64f38d777e3b5911e7f30453fc7b78b3ac2aebadf6cec06757ddd4f290f830a720f905d4fc4aa5628ef'
---
+++ impro/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Impro::Application.config.session_store :cookie_store, key: '_impro_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Impro::Application.config.session_store :active_record_store

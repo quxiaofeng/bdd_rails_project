bfdd143f21a21b0f84714bcf13c1adbeb4bd83fa
¨Xiaofeng <quxiaofeng.at.polyu@gmail.com>
2012-12-14 14:23:48 +0800

    add the example code
    Signed-off-by: ¨Xiaofeng <¨quxiaofeng.at.polyu@gmail.com¨>

---
+++ usermanager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usermanager::Application.config.secret_token = 'b1edf970b0e8188de90fafc49b0f0cde0a3ae1f37e50e26a0cb24b489ecadfcafb9b71ec4a720348eb8431a2fb3da512e528c92b578154248e4091e7dfed46a5'
